ef279b8b33de38645207e1c953109c028a8e03de
Juĺia Sperandio <juhsperandio@hotmail.com>
2020-07-20 21:41:24 -0300

    funcionando - falta condiçao de vitoria e function verifica

--- script.js
+++ script.js
@@ -27,13 +27,14 @@ function MapLab() { // função que cria o mapa
 
         for (colY = 0; colY < map[linX].length; colY++) { //for que constrói as colunas
             let cellId = linX + "." + colY;
+            let paredeId = "parede" + linX;
             //let paredeId = "p." + linX + "." + colY;
 
             switch (map[linX][colY]) {
                 case "W":
                     let parede = document.createElement("div");
                     parede.className = "parede";
-                    parede.id = cellId;
+                    parede.id = paredeId;
                     linha1.appendChild(parede);
                     break;
 
@@ -68,52 +69,69 @@ let divStart = document.getElementById("start");
 let jogador = document.getElementById("jogador");
 divStart.appendChild(jogador);
 
-function move() {
+let linha = 9;
+let coluna = 0;
 
-    let linha = 9;
-    let coluna = 0;
+// function verifica() {
+//     let parede = document.getElementsByClassName("parede");
+//     let novaDiv = document.getElementById(linha + "." + coluna);
+
+//     if (novaDiv == parede) {
+//         false;
+//     }
+// }
+
+function move() {
 
     document.addEventListener('keydown', (event) => {
         const keyName = event.key;
+        // let vazio = document.getElementsByClassName("vazio")
         //console.log(keyName)
+
         if (keyName === "ArrowRight") {
             console.log(linha + "." + coluna);
             coluna += 1;
-            let novaDiv = document.getElementById(linha + "." + coluna);
-            novaDiv.appendChild(jogador);
+            let novaDiv1 = document.getElementById(linha + "." + coluna);
+            novaDiv1.appendChild(jogador);
 
-        } else if (
-            keyName === "ArrowLeft") {
+
+        } else if (keyName === "ArrowLeft") {
             console.log(linha + "." + coluna);
             coluna -= 1;
-            let novaDiv = document.getElementById(linha + "." + coluna);
-            novaDiv.appendChild(jogador);
+            let novaDiv2 = document.getElementById(linha + "." + coluna);
+            novaDiv2.appendChild(jogador);
 
-        } else if (
-            keyName === "ArrowUp") {
+        } else if (keyName === "ArrowUp") {
             console.log(linha + "." + coluna);
             linha -= 1;
-            let novaDiv = document.getElementById(linha + "." + coluna);
-            novaDiv.appendChild(jogador);
-        } else if (
-            keyName === "ArrowDown") {
+            let novaDiv3 = document.getElementById(linha + "." + coluna);
+            novaDiv3.appendChild(jogador);
+
+        } else if (keyName === "ArrowDown") {
             console.log(linha + "." + coluna);
             linha += 1;
-            let novaDiv = document.getElementById(linha + "." + coluna);
-            novaDiv.appendChild(jogador);
+            let novaDiv4 = document.getElementById(linha + "." + coluna);
+            novaDiv4.appendChild(jogador);
         }
     })
 }
+
 move();
 
+let novaDiv = document.getElementById(linha + "." + coluna);
 
+// function vitoria() {
+//     //let fim = document.getElementById("fim");
 
+//     if (novaDiv.textContent == "F") {
+//         console.log("victory")
 
+//     }
 
-
+// }
 
 //for para construir as linhas e outro dentro para construir as colunas - ok
 // inserir movimentos pelo teclado (KeyboardEvents) - ok
-// movimentar a div - colocar uma div jogador que quando mover teclado faz appendChild na div de destino (tecla)
+// movimentar a div - colocar uma div jogador que quando mover teclado faz appendChild na div de destino - ok
 // verificar se é parede ou não
 // estilizar com Css e adicionar imagem
